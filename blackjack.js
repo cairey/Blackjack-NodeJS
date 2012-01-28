835565e5d3fe9a19921aca1c85a3f718d5d5ee8e
cairey <cairey01@hotmail.com>
2012-01-28 15:26:33 +0000

    blackjack using a stack array to simulate a deck of playing cards

--- blackjack.js
+++ blackjack.js
@@ -1,12 +1,41 @@
-function Card(displayValue, suit, value) {
-    this.displayValue;
-    this.suit;
-    this.value
+var options = {};
+
+function Hand() {
+    console.log('creating players hand');
+    this.cards = [];
+}
+
+Hand.prototype.total = function () {
+    console.log('getting total');
+    var score = 0;
+
+    for (var i = 0; i < this.cards.length; i++) {
+        score += this.cards[i].value;
+    }
+
+    return score;
+};
+
+Hand.prototype.bust = function () {
+    console.log('is bust');
+    return this.total() > 21;
+};
+
+Hand.prototype.dealCard = function (card) {
+    console.log('dealing card');
+    console.log(card);
+    this.cards.push(card);
 };
 
 
-Card.prototype.IsAce(){
-    if(this.displayValue == 'Ace'){
+function Card(displayValue, suit, value) {
+    this.displayValue = displayValue;
+    this.suit = suit;
+    this.value = value;
+}
+
+Card.prototype.isAce = function () {
+    if (this.displayValue == 'Ace') {
         return true;
     }
 
@@ -15,26 +44,55 @@ Card.prototype.IsAce(){
 
 
 
-function DeckGenerator() {
+function DeckGenerator(howManyDecksOfCards) {
 
-var values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-var deck = [];
+    var values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+    var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    var deck = [];
 
-for (var v = 1; v < values.length + 1; v++) {
-    
-    for (var s = 0; s < suits.length; s++) {
-            deck.push(new Card(values[v], suits[s], v);
+    for (var d = 0; d < howManyDecksOfCards; d++) {
+        for (var v = 0; v < values.length; v++) {
+
+            for (var s = 0; s < suits.length; s++) {
+                deck.push(new Card(values[v], suits[s], v + 1));
+            }
         }
     }
-
     return deck;
+}
+
+
+function Deck() {
+    console.log('creating new deck');
+    this.deckStack = DeckGenerator(options.howManyDeckOfCards);
+}
+
+Deck.prototype.shuffle = function () {
+    this.deckStack = shuffle(this.deckStack);
+};
+
+Deck.prototype.getCard = function () {
+    var card = this.deckStack.pop();
+    console.log('Getting card from deck');
+    console.log(card);
+    console.log('Dealer has remaining cards: ' + this.deckStack.length);
+    return card;
+};
+
+exports.Game = function (opts) {
+    console.log('creating game');
+    options = opts;
+
+    this.deck = new Deck();
+    this.playersHand = new Hand();
+    this.dealersHand = new Hand();
+}
+
+
+shuffle = function (o) {
+    for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
 };
 
 
-exports.Deck = function(numberOfDecks) {
 
-    var deck = DeckGenerator();
-    return deck;
-
-};
--- server.js
+++ server.js
@@ -18,11 +18,46 @@ application.configure(function() {
 	application.set('view options', {layout:false});
 });
 
+var game;
 
-application.get('/', function(req, res){
+application.get('/', function (req, res) {
 
-	var deck = new blackjack.Deck();
-	res.render('index.jade', deck);
+    var decksToPlayWith = 4;
+    game = new blackjack.Game({ howManyDeckOfCards: decksToPlayWith });
+    game.deck.shuffle();
+    game.playersHand.dealCard(game.deck.getCard());
+    game.playersHand.dealCard(game.deck.getCard());
+
+    console.log(game.playersHand.total());
+    console.log(game.playersHand);
+
+    res.render('index', { model: { playersHand: game.playersHand, dealersHand: game.dealersHand} });
 });
+
+
+application.get('/twist', function (req, res) {
+
+    game.playersHand.dealCard(game.deck.getCard());
+
+    console.log(game.playersHand.total());
+    console.log(game.playersHand);
+
+    res.render('index', { model: { playersHand: game.playersHand, dealersHand: game.dealersHand} });
+});
+
+application.get('/stick', function (req, res) {
+
+    game.dealersHand.dealCard(game.deck.getCard());
+
+    while(game.dealersHand.total() <= 12){
+        game.dealersHand.dealCard(game.deck.getCard());
+    }
+
+    console.log(game.dealersHand.total());
+    console.log(game.playersHand.total());
+
+    res.render('stick', { model: { playersHand: game.playersHand, dealersHand: game.dealersHand } });
+});
+
 
 application.listen(8000);
